75db5aac60dde0a4a9e09350c8cbfbaebe1917c4
Singh_Shailesh <shaileshsingh12396@gmail.com>
2024-07-02 12:19:20 +0530

    Ok

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     DB_NAME: process.env.DB_NAME || "pedalstart",
-    DB_URL: process.env.DB_URL || "mongodb+srv://shaileshsingh12396:XRANUdIrN9GK0e5e@cluster0.1wqh6ks.mongodb.net/ pedalstart?retryWrites=true&w=majority&appName=Cluster0"
+    DB_URL: process.env.DB_URL || "mongodb+srv://shaileshsingh12396:XRANUdIrN9GK0e5e@cluster0.1wqh6ks.mongodb.net/pedalstart?retryWrites=true&w=majority&appName=Cluster0"
 };
