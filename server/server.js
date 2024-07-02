13d0da68df03121246122683b36aef8c5e81fc41
Singh_Shailesh <shaileshsingh12396@gmail.com>
2024-07-02 10:53:41 +0530

    Testing

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const db_config = require("./config/db.config")
 const taskRoutes = require('./routes/tasks');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 
 app.use(express.json());
