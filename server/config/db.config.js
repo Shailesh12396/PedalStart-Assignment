b8f91e9511fcfeb5e75632dc2a53f4c16fdcfd91
Singh_Shailesh <shaileshsingh12396@gmail.com>
2024-07-03 00:02:24 +0530

    1

--- server/config/db.config.js
+++
@@ -1,4 +0,0 @@
-module.exports={
-    DB_NAME: "pedalstart",
-    DB_URL :"mongodb+srv://shaileshsingh12396:XRANUdIrN9GK0e5e@cluster0.1wqh6ks.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-}
