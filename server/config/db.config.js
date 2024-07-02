8d30cdb3d379a3b56fffc2420a3d678c476deefb
Singh_Shailesh <shaileshsingh12396@gmail.com>
2024-07-02 10:36:42 +0530

    Testing

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,4 +1,4 @@
-module.exports={
-    DB_NAME: "pedalstart",
-    DB_URL :"mongodb://127.0.0.1/pedalstart"
-}
+module.exports = {
+    DB_NAME: process.env.DB_NAME || "pedalstart",
+    DB_URL: process.env.DB_URL || "mongodb://127.0.0.1/pedalstart"
+};
